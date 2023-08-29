bca531c7ceda9defa1ad2f344497ea15de1fb21f
KonstantinPanov <33126080+KonstantinPanov@users.noreply.github.com>
2023-08-29 15:58:42 +0300

    Add files via upload

--- src/sql/CTE user_group_log.sql
+++ src/sql/CTE user_group_log.sql
@@ -1,5 +1,5 @@
 with user_group_log as (
-	select luga.hk_group_id, count(*) as cnt_added_users
+	select luga.hk_group_id, count(DISTINCT luga.hk_user_id) as cnt_added_users
 	from STV230547__DWH.l_user_group_activity luga
 	inner join STV230547__DWH.s_auth_history sah on luga.hk_l_user_group_activity = sah.hk_l_user_group_activity 
 	where sah.event ='add' and luga.hk_user_id IN (select hu.hk_user_id from STV230547__DWH.h_users hu order by hu.registration_dt desc limit 10)
