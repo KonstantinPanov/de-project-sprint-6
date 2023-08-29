43c680c588de5700e2d680d63497dbd716c3629e
KonstantinPanov <33126080+KonstantinPanov@users.noreply.github.com>
2023-08-29 15:33:50 +0300

    Add files via upload

---
+++ src/sql/CTE user_group_log.sql
@@ -0,0 +1,15 @@
+with user_group_log as (
+	select luga.hk_group_id, count(*) as cnt_added_users
+	from STV230547__DWH.l_user_group_activity luga
+	inner join STV230547__DWH.s_auth_history sah on luga.hk_l_user_group_activity = sah.hk_l_user_group_activity 
+	where sah.event ='add' and luga.hk_user_id IN (select hu.hk_user_id from STV230547__DWH.h_users hu order by hu.registration_dt desc limit 10)
+	GROUP BY luga.hk_group_id
+)
+
+select hk_group_id
+            ,cnt_added_users
+from user_group_log
+order by cnt_added_users
+limit 10
+
+; 
