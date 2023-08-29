1aa8bc8d28c7aea952b280ff1c93720618906402
KonstantinPanov <33126080+KonstantinPanov@users.noreply.github.com>
2023-08-29 14:37:31 +0300

    Add files via upload

---
+++ src/sql/CTE user_group_messages.sql
@@ -0,0 +1,14 @@
+with user_group_messages as (
+	select lgd.hk_group_id, count(DISTINCT lum.hk_user_id) as cnt_users_in_group_with_messages
+	from  STV230547__DWH.l_groups_dialogs lgd
+	inner join STV230547__DWH.l_user_message lum on lum.hk_message_id = lgd.hk_message_id 
+	GROUP BY lgd.hk_group_id
+)
+
+select hk_group_id,
+            cnt_users_in_group_with_messages
+from user_group_messages
+order by cnt_users_in_group_with_messages
+limit 10
+
+; 
